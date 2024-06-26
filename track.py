3eb572d373352792e06a9b9343fe024edf886784
Taran-0107 <132281589+Taran-0107@users.noreply.github.com>
2024-06-26 16:37:07 +0530

    Create track.py

---
+++ track.py
@@ -0,0 +1,37 @@
+import cv2
+
+from ultralytics import YOLO, solutions
+
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture("cctv_stock.mp4")
+assert cap.isOpened(), "Error reading video file"
+w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+
+# Define line points
+line_points = [(w//2, 0), (w//2,h)]
+
+# Video writer
+video_writer = cv2.VideoWriter("object_counting_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+
+# Init Object Counter
+counter = solutions.ObjectCounter(
+    view_img=True,
+    reg_pts=line_points,
+    classes_names=model.names,
+    draw_tracks=True,
+    line_thickness=2,
+)
+
+while cap.isOpened():
+    success, im0 = cap.read()
+    if not success:
+        print("Video frame is empty or video processing has been successfully completed.")
+        break
+    tracks = model.track(im0, persist=True, show=False)
+
+    im0 = counter.start_counting(im0, tracks)
+    video_writer.write(im0)
+
+cap.release()
+video_writer.release()
+cv2.destroyAllWindows()
